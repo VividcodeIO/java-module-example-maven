a111f1818d43282b566745258b198aa994136e26
Fu Cheng <alexcheng1982@gmail.com>
2019-12-10 23:49:06 +1300

    Add logging

--- persistence-file/src/main/java/io/vividcode/advancedjava/module/persistence/file/FilePersistenceService.java
+++ persistence-file/src/main/java/io/vividcode/advancedjava/module/persistence/file/FilePersistenceService.java
@@ -2,11 +2,14 @@ package io.vividcode.advancedjava.module.persistence.file;
 
 import io.vividcode.advancedjava.module.model.Entity;
 import io.vividcode.advancedjava.module.persistence.PersistenceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FilePersistenceService implements PersistenceService {
+  private static final Logger logger = LoggerFactory.getLogger(FilePersistenceService.class);
 
   @Override
   public void save(Entity entity) {
-    System.out.println("Save to file: " + entity);
+    logger.info("Saved to file: " + entity);
   }
 }
--- persistence-file/src/main/java/module-info.java
+++ persistence-file/src/main/java/module-info.java
@@ -3,5 +3,6 @@ import io.vividcode.advancedjava.module.persistence.file.FilePersistenceService;
 
 module io.vividcode.advancedjava.module.persistence.file {
   requires transitive io.vividcode.advancedjava.module.persistence;
+  requires org.slf4j;
   provides PersistenceService with FilePersistenceService;
 }
